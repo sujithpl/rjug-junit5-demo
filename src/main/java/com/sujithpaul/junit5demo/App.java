6957be07f327e046c2a8480ff231b6463421b08e
Sujith Paul <sujithpl@hotmail.com>
2019-02-16 13:19:33 -0500

    Start with a Maven Quickstart Archetype (version 1.4) project
    - Note that the project uses Java 1.7 and JUnit 4
    - Run `mvn clean install` and fix any issues you see

---
+++ src/main/java/com/sujithpaul/junit5demo/App.java
@@ -0,0 +1,13 @@
+package com.sujithpaul.junit5demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
