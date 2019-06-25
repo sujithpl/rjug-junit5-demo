0686eab4231c1e09cef8573c895892b1585aecaa
Sujith Paul <sujithpl@hotmail.com>
2019-06-24 21:07:17 -0400

    Use AssertAll for grouped assertions
    - In a grouped assertion all assertions are executed, and all failures
    will be reported together

--- src/test/java/com/sujithpaul/junit5demo/ListUtilsTest.java
+++ src/test/java/com/sujithpaul/junit5demo/ListUtilsTest.java
@@ -1,5 +1,6 @@
 package com.sujithpaul.junit5demo;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
@@ -22,90 +23,90 @@ import org.junit.jupiter.api.Test;
  */
 @DisplayName("Tests for ListUtils")
 public class ListUtilsTest {
-    private static final String a = "a";
-    private static final String b = "b";
-    private static final String c = "c";
-    private static final String d = "d";
-    private static final String e = "e";
+	private static final String a = "a";
+	private static final String b = "b";
+	private static final String c = "c";
+	private static final String d = "d";
+	private static final String e = "e";
 
-    private String[] fullArray;
-    private List<String> fullList;
+	private String[] fullArray;
+	private List<String> fullList;
 
-    @BeforeEach
-    void setUp() {
-        fullArray = new String[]{a, b, c, d, e};
-        fullList = new ArrayList<String>(Arrays.asList(fullArray));
-    }
+	@BeforeEach
+	void setUp() {
+		fullArray = new String[] { a, b, c, d, e };
+		fullList = new ArrayList<String>(Arrays.asList(fullArray));
+	}
 
-    /**
-     * Tests intersecting a non-empty list with an empty list.
-     */
-    @Test
-    void testIntersectNonEmptyWithEmptyList() {
-        final List<String> empty = Collections.<String>emptyList();
-        assertTrue(ListUtils.intersection(empty, fullList).isEmpty(), "result not empty");
-    }
+	/**
+	 * Tests intersecting a non-empty list with an empty list.
+	 */
+	@Test
+	void testIntersectNonEmptyWithEmptyList() {
+		final List<String> empty = Collections.<String>emptyList();
+		assertTrue(ListUtils.intersection(empty, fullList).isEmpty(), "result not empty");
+	}
 
-    /**
-     * Tests intersecting a empty list with an empty list.
-     */
-    @Test
-    void testIntersectEmptyWithEmptyList() {
-        final List<?> empty = Collections.EMPTY_LIST;
-        assertTrue(ListUtils.intersection(empty, empty).isEmpty(), "result not empty");
-    }
+	/**
+	 * Tests intersecting a empty list with an empty list.
+	 */
+	@Test
+	void testIntersectEmptyWithEmptyList() {
+		final List<?> empty = Collections.EMPTY_LIST;
+		assertTrue(ListUtils.intersection(empty, empty).isEmpty(), "result not empty");
+	}
 
-    /**
-     * Tests intersecting a non-empty list with an subset of itself.
-     */
-    @Test
-    void testIntersectNonEmptySubset() {
-        // create a copy
-        final List<String> other = new ArrayList<String>(fullList);
+	/**
+	 * Tests intersecting a non-empty list with an subset of itself.
+	 */
+	@Test
+	void testIntersectNonEmptySubset() {
+		// create a copy
+		final List<String> other = new ArrayList<String>(fullList);
 
-        // remove a few items
-        assertNotNull(other.remove(0));
-        assertNotNull(other.remove(1));
+		// remove a few items
+		assertNotNull(other.remove(0));
+		assertNotNull(other.remove(1));
 
-        // make sure the intersection is equal to the copy
-        assertEquals(other, ListUtils.intersection(fullList, other));
-    }
+		// make sure the intersection is equal to the copy
+		assertEquals(other, ListUtils.intersection(fullList, other));
+	}
 
-    /**
-     * Tests intersecting a non-empty list with a list of different types.
-     */
-    @Test
-    void testIntersectListWithNoOverlapAndDifferentTypes() {
-        @SuppressWarnings("boxing")
-        final List<Integer> other = Arrays.asList(1, 23);
-        assertTrue(ListUtils.intersection(fullList, other).isEmpty());
-    }
+	/**
+	 * Tests intersecting a non-empty list with a list of different types.
+	 */
+	@Test
+	void testIntersectListWithNoOverlapAndDifferentTypes() {
+		@SuppressWarnings("boxing")
+		final List<Integer> other = Arrays.asList(1, 23);
+		assertTrue(ListUtils.intersection(fullList, other).isEmpty());
+	}
 
-    /**
-     * Tests intersecting a non-empty list with itself.
-     */
-    @Test
-    void testIntersectListWithSelf() {
-        assertEquals(fullList, ListUtils.intersection(fullList, fullList));
-    }
+	/**
+	 * Tests intersecting a non-empty list with itself.
+	 */
+	@Test
+	void testIntersectListWithSelf() {
+		assertEquals(fullList, ListUtils.intersection(fullList, fullList));
+	}
 
-    /**
-     * Tests intersecting two lists in different orders.
-     */
-    @Test
-    void testIntersectionOrderInsensitivity() {
-        final List<String> one = new ArrayList<String>();
-        final List<String> two = new ArrayList<String>();
-        one.add("a");
-        one.add("b");
-        two.add("a");
-        two.add("a");
-        two.add("b");
-        two.add("b");
-        assertEquals(ListUtils.intersection(one,two),ListUtils.intersection(two, one));
-    }
+	/**
+	 * Tests intersecting two lists in different orders.
+	 */
+	@Test
+	void testIntersectionOrderInsensitivity() {
+		final List<String> one = new ArrayList<String>();
+		final List<String> two = new ArrayList<String>();
+		one.add("a");
+		one.add("b");
+		two.add("a");
+		two.add("a");
+		two.add("b");
+		two.add("b");
+		assertEquals(ListUtils.intersection(one, two), ListUtils.intersection(two, one));
+	}
 
-    @Test
+	@Test
 	@DisplayName("Test emptyIfNull")
 	public void testEmptyIfNull() {
 		assertTrue(ListUtils.emptyIfNull(null).isEmpty());
@@ -134,10 +135,10 @@ public class ListUtilsTest {
 		assertEquals(true, a.equals(b));
 		assertEquals(true, ListUtils.isEqualList(a, b));
 		a.clear();
-		assertEquals(false, ListUtils.isEqualList(a, b));
-		assertEquals(false, ListUtils.isEqualList(a, null));
-		assertEquals(false, ListUtils.isEqualList(null, b));
-		assertEquals(true, ListUtils.isEqualList(null, null));
+		assertAll(() -> assertEquals(false, ListUtils.isEqualList(a, b)),
+				() -> assertEquals(false, ListUtils.isEqualList(a, null)),
+				() -> assertEquals(false, ListUtils.isEqualList(null, b)),
+				() -> assertEquals(true, ListUtils.isEqualList(null, null)));
 	}
 
 	@Test
